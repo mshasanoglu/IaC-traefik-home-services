5a92d889dc11e57440768d51695191b9843d951d
mshasanoglu <mshasanoglu@icloud.com>
2022-03-28 21:12:22 +0000

    fixed nextcloud warnings

--- IaC.sh
+++ IaC.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
 docker-compose up -d cloud-app
+docker-compose exec cloud-app apt -y update
+docker-compose exec cloud-app apt -y install libmagickcore-6.q16-6-extra
 sleep 30
 sed -i "/);/i \  'default_phone_region' => 'DE'," data/cloud-app/config/config.php
