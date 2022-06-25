0aaf3fbfb84534736c33b4b201607648c9006538
mshasanoglu <mshasanoglu@icloud.com>
2022-06-26 01:28:13 +0200

    implemented fastapi

--- IaC-UPDATER.sh
+++ IaC-UPDATER.sh
@@ -30,15 +30,41 @@ docker-compose up -d
 printf "\n${PURPLE} ALL SERVICES UPDATED \n\n${NC}"
 
 # update the cloud container to install missing package on nextcloud
-printf "\n${PURPLE} UPDATING CONTAINER PACKEGES AND INSTALLING REQUIRED PACKAGES FOR NEXTCLOUD \n\n${NC}"
-docker-compose exec cloud-app apt -y update
-docker-compose exec cloud-app apt -y install libmagickcore-6.q16-6-extra
-printf "\n${PURPLE} REQUIRED PACKAGES FOR NEXTCLOUD INSTALLED \n\n${NC}"
+printf "\n${PURPLE} UPDATING CONTAINER PACKEGES AND INSTALLING REQUIRED PACKAGES FOR NEXTCLOUD IF NEEDED \n\n${NC}"
+REQUIRED_PKG="libmagickcore-6.q16-6-extra"
+PKG_OK=$(docker-compose exec cloud-app dpkg-query -W --showformat='${Status}\n' $REQUIRED_PKG|grep "install ok installed")
+
+if [ "" = "$PKG_OK" ]; then
+  echo "No $REQUIRED_PKG. Setting up $REQUIRED_PKG."
+  docker-compose exec cloud-app apt -y update
+  docker-compose exec cloud-app apt -y install $REQUIRED_PKG
+  printf "\n${PURPLE} REQUIRED PACKAGES FOR NEXTCLOUD INSTALLED \n\n${NC}"
+fi
 
 # append extra configuration values
-printf "\n${PURPLE} ADDING EXTRA VALUES AND RESTARTING NEXTCLOUD CONTAINER\n\n${NC}"
-docker-compose exec --user www-data cloud-app /var/www/html/occ config:app:set text workspace_available --value=0
-docker-compose exec cloud-app sed -i "/opcache.interned_strings_buffer/c\opcache.interned_strings_buffer=32" /usr/local/etc/php/conf.d/opcache-recommended.ini
-docker-compose restart cloud-app 
+printf "\n${PURPLE} ADDING EXTRA VALUES AND RESTARTING NEXTCLOUD CONTAINER IF NEEDED \n\n${NC}"
+WORKSPACE_AVAILABLE=$(docker-compose exec --user www-data cloud-app /var/www/html/occ config:app:get text workspace_available | head -c1)
+INTERNED_STRINGS_BUFFER=$(docker-compose exec cloud-app awk -F "=" '/opcache.interned_strings_buffer/ {print $2}' /usr/local/etc/php/conf.d/opcache-recommended.ini | head -c2)
+VALUES_CHANGED=false
+
+if [ $WORKSPACE_AVAILABLE != 0 ]; then
+  docker-compose exec --user www-data cloud-app /var/www/html/occ config:app:set text workspace_available --value=0
+  VALUES_CHANGED=true
+fi
+if [ $INTERNED_STRINGS_BUFFER != 32 ]; then
+  docker-compose exec cloud-app sed -i "/opcache.interned_strings_buffer/c\opcache.interned_strings_buffer=32" /usr/local/etc/php/conf.d/opcache-recommended.ini
+  VALUES_CHANGED=true
+fi
+if [ $VALUES_CHANGED = true ]; then
+  docker-compose restart cloud-app 
+  printf "\n${PURPLE} REQUIRED VALUES FOR NEXTCLOUD ADDED \n\n${NC}"
+fi
 
 printf "\n${CYAN} PIPELINE DONE \n\n${NC}"
+
+
+
+
+
+
+
